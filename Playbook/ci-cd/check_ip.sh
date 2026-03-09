09e881dcde4350f22c531faaa0b5c05d3f762ddd
dungphung411 <mdungphung411@gmail.com>
2026-03-09 10:26:32 +0700

    feat: Add Ansible playbooks for building Nginx packages and for CI/CD network utility installation and IP checking.

---
+++ Playbook/ci-cd/check_ip.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Script to check IP/network status using netstat
+echo "Checking network status..."
+netstat -rn
---
+++ Playbook/ci-cd/install_net_tools.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Script to install net-tools
+echo "Installing net-tools..."
+sudo apt-get update && sudo apt-get install -y net-tools
